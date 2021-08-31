0c847d0cb78efd14018304fe64a5c6a1a961c934
donald <30538196+KnownAsDon@users.noreply.github.com>
2021-08-31 22:37:59 +0200

    Update

--- JS/script.js
+++ JS/script.js
@@ -19,6 +19,10 @@ let ua = navigator.userAgent;
 let androidversion;
 
 window.onload = function() {
+    if (document.getElementById("android-version") == 'undefined' || document.getElementById("android-version") == null) {
+        return;
+    }
+
     if (ua.indexOf("Android") >= 0 ) {
         androidversion = parseFloat(ua.slice(ua.indexOf("Android")+8)).toFixed(1);
         if (androidversion >= 5.0) {
@@ -55,22 +59,4 @@ window.onload = function() {
         document.getElementById("android-version").innerHTML = "Not Android";
     }
     return androidversion;
-}
-
-function searchFile() {
-    let i, a, txtValue;
-    let input = document.getElementById('fm-search');
-    let filter = input.value.toUpperCase();
-    let ul = document.getElementById('launchers-ul');
-    let li = document.getElementsByTagName('li');
-
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-        } else {
-          li[i].style.display = "none";
-        }
-      }
 }
